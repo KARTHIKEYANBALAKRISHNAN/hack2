2c6ea0395165eecbf01b8fcedc34f2ed0559b51a
minakshi <minakshiagg129@gmail.com>
2015-08-28 03:59:45 -0700

    Minakshi- changes for email

--- Web/TestTwilio.aspx.cs
+++ Web/TestTwilio.aspx.cs
@@ -5,17 +5,18 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Twilio;
-
+using ActiveUp.Net.Mail;
+using PT.MailIntegration.IMAP;
 namespace Web
 {
     public partial class TestTwilio : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            string smsID = SendSMS();
-            smsid.InnerText = smsID;
+                                   
         }
-
+        
         public string SendSMS()
         {
             //var call = twilio.InitiateOutboundCall("+919962971796", "+1919962971796", "http://example.com/handleCall");
@@ -24,6 +25,19 @@ namespace Web
             var twilio = new TwilioRestClient(AccountSid, AuthToken);
             var message = twilio.SendMessage("+919962071729", "+919962979196", "Twilio");
             return message.Sid;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MailRepository rep = new MailRepository("imap.gmail.com", 993, true, @TextBox1.Text, TextBox2.Text);
+            foreach (ActiveUp.Net.Mail.Message email in rep.GetUnreadMails("Inbox"))
+            {
+                string from = email.From.ToString();
+                string subj = email.Subject;
+                emails.InnerText = from + subj;
+                SendSMS();
+             }
+
         }//SendSMS
     }
 }
--- Web/TestTwilio.aspx.designer.cs
+++ Web/TestTwilio.aspx.designer.cs
@@ -29,5 +29,41 @@ namespace Web {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl smsid;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// emails control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl emails;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
